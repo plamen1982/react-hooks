88bae43d253d533abbf5361ddee05dc8dd7d9095
plamen1982 <plamen.hristov@ymail.com>
2019-07-18 22:21:49 +0300

    Avoiding Props Drilling with useContext hook and React Context

--- hooks-todos/src/App.js
+++ hooks-todos/src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { UserContext } from './index';
 
 export default function App() {
-  return <div>App</div>
+  const value = useContext(UserContext);
+  return <div>Hi, {value}</div>
 }
--- hooks-todos/src/index.js
+++ hooks-todos/src/index.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+export const UserContext = createContext();
+const username = "Dave";
+
+ReactDOM.render(
+<UserContext.Provider value={username}>
+    <App />
+</UserContext.Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
