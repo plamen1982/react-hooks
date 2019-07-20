57d61362bebd9c10258362e0ea56b8001176bacb
plamen1982 <plamen.hristov@ymail.com>
2019-07-20 09:23:57 +0300

    Styling our TodoList Component with TailwindCSS

--- hooks-todos/src/components/TodoList.js
+++ hooks-todos/src/components/TodoList.js
@@ -3,13 +3,37 @@ import TodosContext from '../context';
 
 export default function TodoList() {
     const { state } = useContext(TodosContext);
-
+    const { todos } = state;
+    const title = todos.length > 0 
+                    ? `${todos.length} Todos`
+                    : `Nothing To Do!`
     return (
-        <div>
-            <ul>
+        <div className="container mx-auto max-w-md text-center font-mono">
+            <h1 className="text-bold">{title}</h1>
+            <ul className="list=reset text-white p-o">
                 {state.todos.map(todo => (
-                    <li key={todo.id}>
-                        <span>{todo.text}</span>
+                    <li 
+                        key={todo.id}
+                        className="flex items-center
+                        bg-orange-600 border-black border-dashed border-2 my-2 py-4"
+                    >
+                        
+                        <span
+                            className="flex-1 ml-12
+                            cursor-pointer"
+                        >{todo.text}</span>
+                        <button>
+                            <img src="https://icon.now.sh/edit/0050c5" 
+                                alt="Edit Icon"
+                                className="h-6"
+                            />
+                        </button>
+                        <button>
+                            <img src="https://icon.now.sh/delete/8b0000" 
+                                alt="Delete Icon"
+                                className="h-6"
+                            />
+                        </button>
                     </li>
                 ))}
             </ul>
