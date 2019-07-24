774f02f3d368bb9435251e4ef35bfb72a7fb18eb
plamen1982 <plamen.hristov@ymail.com>
2019-07-24 09:46:51 +0300

    Created search functionality

--- hooks-news/src/App.js
+++ hooks-news/src/App.js
@@ -1,7 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 export default function App() {
+  const [results, setResults] = useState([]);
+  const [query, setQuery] = useState('reacthooks');
+  
+  function handleSearch(event) {
+    const { target: { value } } = event;
+    setQuery(value);
+  }
+
+  useEffect(() => {
+    axios
+      .get(`https://hn.algolia.com/api/v1/search?query=${query}`)
+      .then(respose => {
+        console.log(respose.data);
+        setResults(respose.data.hits)
+      });
+  }, [query]);
+
   return(
-    <div>App</div>
+    <>
+      <input 
+        placeholder="Search for news"
+        onChange={handleSearch}
+      />
+    <div>
+      <ul>
+        {results.map(result => (
+          <li key={result.objectID}>
+            {result.title}
+          </li>
+          )
+        )}
+
+      </ul>
+    </div>
+    </>
   );
 }
