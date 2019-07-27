f90ef61ec5898f7a7b871e65d0c1b32c92a3019a
plamen1982 <plamen.hristov@ymail.com>
2019-07-27 11:33:55 +0300

    Finishing the project added errors, clear button and css

--- hooks-news/src/App.js
+++ hooks-news/src/App.js
@@ -1,50 +1,81 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import axios from 'axios';
 
 export default function App() {
-  const [results, setResults] = useState([]);
-  const [query, setQuery] = useState('reacthooks');
+    const [results, setResults] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [query, setQuery] = useState('');
+    const [error, setError] = useState(false);
+    const searchElement = useRef(null);
 
-  function handleSearch(event) {
-    const { target: { value } } = event;
-    setQuery(value);
-  }
+    function handleSubmit(event) {
+      event.preventDefault();
+        getNews();
+    }
 
-  function handleSubmit() {
-    getNews();
-  }
+    function handleSearch(event) {
+      const { target: { value } } = event;
+      setQuery(value);
+    }
 
-  async function getNews() {
-   const results = await axios
-    .get(`https://hn.algolia.com/api/v1/search?query=${query}`)
-    setResults(results.data.hits);
-  }
+    function handleReset() {
+      setQuery('');
+      searchElement.current.focus();
+    }
 
-  useEffect(() => {
-    getNews();
-  }, []);
+    function getNews() {
+        setLoading(true);
+        axios
+            .get(`https://hn.algolia.com/api/v1/search?query=${query}`)
+            .then(results => {
+                setResults(results.data.hits);
+                setLoading(false);
+            })
+            .catch(error => {
+                console.log(error);
+                setLoading(false);
+                setError(true)
+            });
+    }
 
-  return(
-    <>
-      <form onSubmit={handleSubmit}>
-        <input 
-          placeholder="Search for news"
-          onChange={handleSearch}
-        />
-        <button onClick={handleSubmit}>
-            Search
-        </button>
-        <div>
-          <ul>
-            {results.map(result => (
-              <li key={result.objectID}>
-                {result.title}
-              </li>
-              )
-            )}
-          </ul>
-        </div>
-      </form>
-    </>
-  );
+    useEffect(() => {
+        setQuery('reacthooks')
+        getNews();
+    }, []);
+
+    return (
+        <>
+            <form onSubmit={handleSubmit}>
+                <input 
+                  placeholder='Search for news' 
+                  onChange={handleSearch} 
+                  ref={searchElement} 
+                  value={query} 
+                  className="border p-1 rounded"
+                />
+                <button
+                  className="bg-teal text-white p-1 rounded m-1">
+                  Search
+                </button>
+                <button 
+                  className="bg-teal text-white p-1 rounded"
+                  onClick={handleReset}
+                >
+                  Clear
+                </button>
+                <div className="font-bold text-orange-dark">
+                    {loading ? (
+                        <div>Loading...</div>
+                    ) : (
+                        <ul>
+                            {results.map(result => (
+                                <li key={result.objectID}>{result.title}</li>
+                            ))}
+                        </ul>
+                    )}
+                    {error && <div className="text-red font-bold">{error.message}</div>}
+                </div>
+            </form>
+        </>
+    );
 }
