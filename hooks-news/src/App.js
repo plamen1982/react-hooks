882bdd466610308306a25a7cb326f9acc892f87d
plamen1982 <plamen.hristov@ymail.com>
2019-07-27 11:04:08 +0300

    Submit request for news with button

--- hooks-news/src/App.js
+++ hooks-news/src/App.js
@@ -4,38 +4,47 @@ import axios from 'axios';
 export default function App() {
   const [results, setResults] = useState([]);
   const [query, setQuery] = useState('reacthooks');
-  
+
   function handleSearch(event) {
     const { target: { value } } = event;
     setQuery(value);
   }
 
+  function handleSubmit() {
+    getNews();
+  }
+
+  async function getNews() {
+   const results = await axios
+    .get(`https://hn.algolia.com/api/v1/search?query=${query}`)
+    setResults(results.data.hits);
+  }
+
   useEffect(() => {
-    axios
-      .get(`https://hn.algolia.com/api/v1/search?query=${query}`)
-      .then(respose => {
-        console.log(respose.data);
-        setResults(respose.data.hits)
-      });
-  }, [query]);
+    getNews();
+  }, []);
 
   return(
     <>
-      <input 
-        placeholder="Search for news"
-        onChange={handleSearch}
-      />
-    <div>
-      <ul>
-        {results.map(result => (
-          <li key={result.objectID}>
-            {result.title}
-          </li>
-          )
-        )}
-
-      </ul>
-    </div>
+      <form onSubmit={handleSubmit}>
+        <input 
+          placeholder="Search for news"
+          onChange={handleSearch}
+        />
+        <button onClick={handleSubmit}>
+            Search
+        </button>
+        <div>
+          <ul>
+            {results.map(result => (
+              <li key={result.objectID}>
+                {result.title}
+              </li>
+              )
+            )}
+          </ul>
+        </div>
+      </form>
     </>
   );
 }
